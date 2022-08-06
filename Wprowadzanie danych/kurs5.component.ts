df09918cf5049d7c20d9b81f08d6fe92f4cdbc47
Gregory <109724010+Gregory900@users.noreply.github.com>
2022-08-06 20:28:51 +0200

    Add files via upload

---
+++ Wprowadzanie danych/kurs5.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-kurs5',
+  templateUrl: './kurs5.component.html',
+  styleUrls: ['./kurs5.component.css']
+})
+export class Kurs5Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  pi=Math.PI;
+
+  dane= new Dane ("Andrzej", "Kowalski");
+
+  funkcja(){
+    return("Jestem Pan " + this.dane.name + " a na nazwisko mam " + this.dane.surname + ".")
+  }
+
+}
+
+class Dane{
+  constructor(public name:string, public surname:string){
+    };
+}
+
+
