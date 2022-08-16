f74391d4b8c29d5f55487c0621076e5180405b4c
Gregory <109724010+Gregory900@users.noreply.github.com>
2022-08-16 11:19:29 +0200

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { LOCALE_ID, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+
+import localePL from '@angular/common/locales/pl'
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localePL);
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
